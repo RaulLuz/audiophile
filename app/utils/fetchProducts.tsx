3d3dd9d65ed77fccdd0173049f09c7cd930cdcc9
Raul Oliveira <raulodl@gmail.com>
2024-05-01 18:16:53 -0300

    fix: data.json updated

--- app/utils/fetchProducts.tsx
+++ app/utils/fetchProducts.tsx
@@ -1,9 +1,7 @@
-import { promises as fs } from "fs";
-
 export default async function fetchProducts() {
   const res = await fetch('https://raw.githubusercontent.com/RaulLuz/audiophile/master/public/data.json?token=GHSAT0AAAAAACREJ7TF5KYWKDCA4UXPAJE2ZRSV5XQ');
 
   const data = await res.json()
 
-  return data;
+  return data.products;
 }
